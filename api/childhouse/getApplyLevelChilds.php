ebaefd6e0037651f6337d15da88280545add5765
KKS <kirill.kuliuckin2016@yandex.ru>
2021-09-26 08:45:42 +0300

    Apply Bloom level childrens

---
+++ api/childhouse/getApplyLevelChilds.php
@@ -0,0 +1,8 @@
+<?php
+$res=[
+    ["name"=>"Иван","secondname"=>"Иванов","patronic"=>"Иванович","subject"=>"Роботостроение"],
+    ["name"=>"Мария","secondname"=>"Орлова","patronic"=>"Витальевна","subject"=>"Маникюр"],
+    ["name"=>"Юрий","secondname"=>"Юрьев","patronic"=>"Алексеевич","subject"=>"Футбол"]
+];
+echo json_encode($res);
+?>
